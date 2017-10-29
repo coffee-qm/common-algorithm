c2735075201ae16630bb1a1ab3a6895239be6025
coffee-qm <coffee.qm@gmail.com>
2017-10-29 22:13:13 +0800

    Sort
    Sort

--- src/main/java/com/coffee/common/algorithm/sort/impl/SelectSort.java
+++ src/main/java/com/coffee/common/algorithm/sort/impl/SelectSort.java
@@ -14,7 +14,7 @@ public class SelectSort extends Sort {
 		//
 		for (int i = 0; i < arr.length - 1; i++) {
 			int min = i;
-			for (int j = i + 1; j < arr.length - 1; j++) {
+			for (int j = i + 1; j < arr.length; j++) {
 				if (arr[j] < arr[min]) {
 					min = j;
 				}
