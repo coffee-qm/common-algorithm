82338906f2991f57b2e7b2fec03be2031eabd80f
coffee-qm <coffee.qm@gmail.com>
2017-10-29 22:34:41 +0800

    Sort
    Sort

--- src/test/java/com/coffee/common/algorithm/sort/SortTest.java
+++ src/test/java/com/coffee/common/algorithm/sort/SortTest.java
@@ -4,13 +4,17 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
+import com.coffee.common.algorithm.sort.impl.BubbleSort;
 import com.coffee.common.algorithm.sort.impl.HeapSort;
+import com.coffee.common.algorithm.sort.impl.InsertionSort;
 import com.coffee.common.algorithm.sort.impl.MergeSort;
 import com.coffee.common.algorithm.sort.impl.QuickSort;
+import com.coffee.common.algorithm.sort.impl.SelectSort;
+import com.coffee.common.algorithm.sort.impl.ShellSort;
 
 public class SortTest {
 	private int[] getArr() {
-		final int[] arr = { 9, 1, 4, 10, 123, 2, 598, 34, 18 };
+		final int[] arr = { 9, 1, 4, 10, 123, 2, 598, 34, 18, 22 };
 		return arr;
 	}
 
@@ -18,7 +22,39 @@ public class SortTest {
 	public void test0() {
 		final int[] arr = getArr();
 		Arrays.sort(arr);
-		System.out.println(Arrays.toString(arr));
+		System.out.println("JDK:" + Arrays.toString(arr));
+	}
+
+	@Test
+	public void testSelectSort() {
+		final int[] arr = getArr();
+		final SelectSort sort = new SelectSort();
+		sort.sort(arr);
+		System.out.println("Select:" + Arrays.toString(arr));
+	}
+
+	@Test
+	public void testBubbleSort() {
+		final int[] arr = getArr();
+		final BubbleSort sort = new BubbleSort();
+		sort.sort(arr);
+		System.out.println("Bubble:" + Arrays.toString(arr));
+	}
+
+	@Test
+	public void testInsertionSort() {
+		final int[] arr = getArr();
+		final InsertionSort sort = new InsertionSort();
+		sort.sort(arr);
+		System.out.println("Insertion:" + Arrays.toString(arr));
+	}
+
+	@Test
+	public void testShellSort() {
+		final int[] arr = getArr();
+		final ShellSort sort = new ShellSort();
+		sort.sort(arr);
+		System.out.println("Shell:" + Arrays.toString(arr));
 	}
 
 	@Test
@@ -26,7 +62,7 @@ public class SortTest {
 		final int[] arr = getArr();
 		final HeapSort sort = new HeapSort();
 		sort.sort(arr);
-		System.out.println(Arrays.toString(arr));
+		System.out.println("Heap:" + Arrays.toString(arr));
 	}
 
 	@Test
@@ -34,7 +70,7 @@ public class SortTest {
 		final int[] arr = getArr();
 		final MergeSort sort = new MergeSort();
 		sort.sort(arr);
-		System.out.println(Arrays.toString(arr));
+		System.out.println("Merge:" + Arrays.toString(arr));
 	}
 
 	@Test
@@ -42,6 +78,6 @@ public class SortTest {
 		final int[] arr = getArr();
 		final QuickSort sort = new QuickSort();
 		sort.sort(arr);
-		System.out.println(Arrays.toString(arr));
+		System.out.println("Quick:" + Arrays.toString(arr));
 	}
 }
